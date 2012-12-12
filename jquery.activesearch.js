89ddb4596fb92072d999f08e93cfdc85ab922ee1
Kennedy Mwenja <mwenja07@gmail.com>
2012-12-12 18:36:02 +0300

    can now specify the content type of data sent

--- jquery.activesearch.js
+++ jquery.activesearch.js
@@ -3,6 +3,7 @@
 		var options = $.extend({
 			'type' : 'GET',
 			'dataType' : 'json',
+			'contentType':'application/x-www-form-urlencoded',
 			'error' : function(xhr, status, error) {
 				console.log("Status: " + status);
 				console.log("Error: " + error);
@@ -18,7 +19,8 @@
 				if(value && value.length > 0) { // TODO: cater for additional filters eg if val > 3 chars
 					$.ajax({ // TODO: cater for progress status
 						url : options.url, // TODO: cater for pretty urls
-						data : options.params(value), // TODO: cater for content type
+						data : options.params(value),
+						contentType: options.contentType,
 						type : options.type,
 						dataType : options.dataType,
 						success : function(data, status, xhr) {
