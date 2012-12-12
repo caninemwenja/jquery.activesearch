401a7bbae83d63c67f6ff3aff539d3b9c8e02d32
Kennedy Mwenja <mwenja07@gmail.com>
2012-12-12 17:53:44 +0300

    cleaning up

--- jquery.activesearch.js
+++ jquery.activesearch.js
@@ -4,7 +4,9 @@
 			'type' : 'GET',
 			'dataType' : 'json',
 			'error' : function(d, e, x) {
-				alert(d + " - " + e + " - " + x);
+				console.log("E: " + e);
+				console.log("X: " + x);
+				console.log("D: " + d);
 			}
 		}, options);
 
@@ -31,42 +33,3 @@
 		});
 	}
 }(jQuery));
-
-$(document).ready(function() {
-	/*$("#keyword_field").keyup(function(event) {
-	 value = $(this).val();
-	 results_div = $("#results");
-	 results_div.html("");
-	 $.ajax({
-	 url : 'search.php',
-	 data : 'keyword=' + value,
-	 type : 'GET',
-	 dataType : 'json',
-	 success : function(data, xhr, status) {
-	 for( i = 0; i < data.length; i++) {
-	 results_div.append("<li>" + data[i] + "</li>");
-	 }
-	 },
-	 error : function(d, e, x) {
-	 alert(d + ":" + e + ":" + x);
-	 }
-	 });
-	 });*/
-
-	$("#keyword_field").activesearch({
-		'url' : 'search.php',
-		'param' : 'keyword',
-		'success' : function(data, xhr, status) {
-			var results_div = $("#results");
-			results_div.html("");
-			for( i = 0; i < data.length; i++) {
-				results_div.append("<li>" + data[i] + "</li>");
-			}
-		},
-		'error' : function(d, e, x) {
-			console.log("E: " + e);
-			console.log("X: " + x);
-			console.log("D: " + d);
-		}
-	});
-});
